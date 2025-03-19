5f85b33582c58f75ac3821c627e49ecc680571e1
yaelGarcia123 <yaklen2@hotmail.com>
2025-03-19 00:50:07 -0700

    trabajo con back

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router); // Inyecta el Router
+
+  // Verifica si el usuario está autenticado
+  const token = localStorage.getItem('token'); // Obtén el token del localStorage
+
+  if (token) {
+    // Si hay un token, el usuario está autenticado
+    return true;
+  } else {
+    // Si no hay token, redirige al usuario a la página de inicio de sesión
+    router.navigate(['/login']);
+    return false;
+  }
 };
